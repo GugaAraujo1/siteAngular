157655ff0dd0da5ea64665a4b8f84bd6c568e583
Gustavo Araujo <gu.araujo16@hotmail.com>
2023-09-24 22:00:07 -0300

    site pronto?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,11 @@ export class AppComponent {
   fotoSupremo = '../../assets/imagens/LancheDoDia.png'
 
   itensCatalogo = [
-    { name: 'Galúrguer Supremo', price: 10.99 },
-    { name: 'Galúrguer de Frango', price: 8.99 },
-    { name: 'Galúrguer Vegetariano', price: 8.99 },
-    { name: 'Galúrguer Picante', price: 8.99 },
-    { name: 'Galúrguer Duplo com Queijo', price: 8.99 },
-    { name: 'Galúrguer Especial do Chef', price: 8.99 },
-    // Adicione mais itens do catálogo aqui
+    { name: 'Galúrguer Supremo', price: 10.99, imagem:'../../assets/imagens/imagemSupremo.jpg'},
+    { name: 'Galúrguer de Frango', price: 8.99, imagem: '../../assets/imagens/imagemFrango.jpg' },
+    { name: 'Galúrguer Vegetariano', price: 8.99, imagem: '../../assets/imagens/imagemVegetariano.jpg' },
+    { name: 'Galúrguer Picante', price: 8.99, imagem: '../../assets/imagens/imagemPicante.jpg' },
+    { name: 'Galúrguer Duplo com Queijo', price: 8.99, imagem: '../../assets/imagens/imagemDuplo.png' },
+    { name: 'Galúrguer Especial do Chef', price: 8.99, imagem: '../../assets/imagens/imagemChef.jpg' },
   ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
-import { ItemOutputComponent } from './item-output/item-output.component';
-import { ItemDropdownComponent } from './item-dropdown/item-dropdown.component';
 import { FormsModule } from '@angular/forms';
 import { PedidosGalurguerComponent } from './pedidos-galurguer/pedidos-galurguer.component';
 import { EdicaoComponent } from './edicao/edicao.component';
@@ -16,8 +14,6 @@ import { HeaderComponent } from './header/header.component';
   declarations: [
     AppComponent,
     ItemDetailComponent,
-    ItemOutputComponent,
-    ItemDropdownComponent,
     PedidosGalurguerComponent,
     EdicaoComponent,
     CatalogoComponent,
